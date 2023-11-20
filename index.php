7e4bf5778719162a71854c7b8952ec4566bae5f7
Gabriel <gabrielfrnz31@gmail.com>
2023-11-20 13:14:51 +0100

    another fix

--- index.php
+++ index.php
@@ -14,11 +14,11 @@ if (isset($_GET["page"])) {
 <html>
 
 <head>
-    <link rel="stylesheet" type="text/css" href="style/style.css">
     <title>Horde Image Indexer</title>
     <script src="js/script.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style/style.css">
 </head>
 
 <body>
