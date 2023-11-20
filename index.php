55a9c8b4d10d7b2cd9ebd9243dcd0965e6f19bad
Gabriel <gabrielfrnz31@gmail.com>
2023-11-20 13:14:34 +0100

    hopefully fix

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (isset($_GET["page"])) {
     <title>Horde Image Indexer</title>
     <script src="js/script.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
@@ -177,5 +177,4 @@ if (isset($_GET["page"])) {
         </ul>
     </div>
 </body>
-
 </html>
