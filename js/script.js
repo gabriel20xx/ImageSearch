4a9b75a59e87db15a64e9cb4599eaa56cff157d1
Gabriel <gabrielfrnz31@gmail.com>
2023-11-20 16:27:14 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
+// Use const and let for variable declarations
+const images = []; // assuming you have an array of images
+
 function setDisplayMode(mode) {
     window.location.href = `index.php?display_mode=${mode}`;
 }
 
-var currentImageIndex = 0; // Index of the currently displayed image
+let currentImageIndex = 0; // Index of the currently displayed image
 
 // Function to open fullscreen with a specific image
 function openFullscreen(imageSrc) {
-    var fullscreenContainer = document.getElementById('fullscreenContainer');
-    var fullscreenImage = document.getElementById('fullscreenImage');
+    const fullscreenContainer = document.getElementById('fullscreenContainer');
+    const fullscreenImage = document.getElementById('fullscreenImage');
 
     fullscreenImage.src = imageSrc;
     fullscreenContainer.style.display = 'flex';
@@ -15,7 +18,7 @@ function openFullscreen(imageSrc) {
 
 // Function to close fullscreen
 function closeFullscreen() {
-    var fullscreenContainer = document.getElementById('fullscreenContainer');
+    const fullscreenContainer = document.getElementById('fullscreenContainer');
     fullscreenContainer.style.display = 'none';
 }
 
@@ -31,59 +34,59 @@ function nextImage() {
     openFullscreen(images[currentImageIndex]);
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    // Use querySelectorAll to select multiple elements and loop through them
+    document.querySelectorAll('.search-form, .model-form, .slider-form').forEach(function (element) {
+        element.style.display = 'none';
+    });
+});
+
 function handleFilterChange(selectedFilter) {
-    var searchElement = document.querySelector('.search-form');
-    var modelElement = document.querySelector('.model-form');
-    var sliderElement = document.querySelector('.slider-form');
+    const searchElement = document.querySelector('.search-form');
+    const modelElement = document.querySelector('.model-form');
+    const sliderElement = document.querySelector('.slider-form');
 
-    searchElement.style.display = 'none';
-    modelElement.style.display = 'none';
-    sliderElement.style.display = 'none';
+    // Use a switch statement for better readability
+    switch (selectedFilter) {
+        case 'PositivePrompt':
+        case 'NegativePrompt':
+        case 'Filename':
+            searchElement.style.display = 'block';
+            break;
 
-    if (selectedFilter === 'PositivePrompt' || selectedFilter === 'NegativePrompt' || selectedFilter === 'Filename') {
-        searchElement.style.display = 'block';
-    }
+        case 'ModelHash':
+        case 'Model':
+        case 'SeedResizeFrom':
+        case 'DenoisingStrength':
+            modelElement.style.display = 'block';
+            break;
 
-    if (selectedFilter === 'ModelHash' || selectedFilter === 'Model' || selectedFilter === 'SeedResizeFrom' || selectedFilter === 'DenoisingStrength') {
-        modelElement.style.display = 'block';
-    }
+        case 'NSFWProbability':
+            sliderElement.style.display = 'block';
+            break;
 
-    if (selectedFilter === 'NSFWProbability') {
-        sliderElement.style.display = 'block';
+        default:
+            break;
     }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    var searchElement = document.querySelector('.search-form');
-    var modelElement = document.querySelector('.model-form');
-    var sliderElement = document.querySelector('.slider-form');
+    console.log('Script is running');
 
-    searchElement.style.display = 'none';
-    modelElement.style.display = 'none';
-    sliderElement.style.display = 'none';
+    // Use event delegation to handle click events on the form
+    document.querySelector('form').addEventListener('click', function (event) {
+        const target = event.target;
 
-
-    console.log("Script is running");
-
-    document.querySelector("form").addEventListener("click", function (event) {
-        var target = event.target;
-        if (target.type === 'submit' && target.classList.contains('btn-primary')) {
-            event.preventDefault();
-            if (target.tagName === 'IMG') {
-                openFullscreen(target.src);
-            }
-        }
-
-        if (target.classList.contains("remove-row")) {
-            console.log("Remove button clicked");
+        if (target.classList.contains('remove')) {
+            console.log('Remove button clicked');
             target.closest('.row').remove();
         }
 
-        if (target.classList.contains("add-row")) {
-            console.log("Add button clicked");
-            var clonedElement = document.querySelector("form > .container:first-child").cloneNode(true);
+        if (target.classList.contains('add')) {
+            console.log('Add button clicked');
+            const clonedElement = document.querySelector('form > .container:first-child').cloneNode(true);
             clonedElement.innerHTML += '<button type="button" class="remove-row btn btn-danger">Remove</button>';
-            document.querySelector("form > .container:last-child").insertAdjacentElement('afterend', clonedElement);
+            document.querySelector('form > .container:last-child').insertAdjacentElement('afterend', clonedElement);
         }
     });
 });
