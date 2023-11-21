b830e7a6bd98915fa320b05f132b0aab6b3fa502
Gabriel <gabrielfrnz31@gmail.com>
2023-11-21 15:57:29 +0100

    fix

--- index.php
+++ index.php
@@ -53,18 +53,18 @@ include 'includes/mysql.php';
             </div>
 
             <div class="oneValueForm">
-                <label for="first-value" class="form-label">Type in your Number...</label>
-                <input class="oneValueInput" id="first-value" name="first-value" type="number" step="0.01">
+                <label for="one-value" class="form-label">Type in your Number...</label>
+                <input class="oneValueInput" id="one-value" name="one-value" type="number" step="0.01" value="<?php echo isset($_GET['one-value']) ? htmlentities($_GET['one-value']) : "0.5"; ?>">
             </div>
 
             <div class="twoValueForm">
                 <div>
                     <label for="lower-value" class="form-label">Type in your lower Number...</label>
-                    <input class="lowerValueInput" id="lower-value" name="lower-value" type="number" step="0.01">
+                    <input class="lowerValueInput" id="lower-value" name="lower-value" type="number" step="0.01" value="<?php echo isset($_GET['lower-value']) ? htmlentities($_GET['lower-value']) : "0.25"; ?>">>
                 </div>
                 <div>
                     <label for="upper-value" class="form-label">Type in your higher Number...</label>
-                    <input class="upperValueInput" id="upper-value" name="upper-value" type="number" step="0.01">
+                    <input class="upperValueInput" id="upper-value" name="upper-value" type="number" step="0.01" value="<?php echo isset($_GET['upper-value']) ? htmlentities($_GET['upper-value']) : "0.75"; ?>">>
                 </div>
             </div>
 
