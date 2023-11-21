4c04e6038d29c2ecef6a7af77bbe265f61905b98
Gabriel <gabrielfrnz31@gmail.com>
2023-11-21 15:54:00 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -68,19 +68,30 @@ function handleFilterChange(selectedFilter) {
   const searchElement = document.querySelector(".search-form");
   const modelElement = document.querySelector(".model-form");
   const minmaxrangeElement = document.querySelector(".minmaxrange-form");
+  const oneValueElement = document.querySelector(".oneValueForm");
+  const twoValueElement = document.querySelector(".twoValueForm");
+
   const searchInput = document.querySelector(".search-input");
   const modelInput = document.querySelector(".model-input");
   const minmaxrangeInput = document.querySelector(".minmaxrange-input");
+  const oneValueInput = document.querySelector(".oneValueInput");
+  const lowerValueInput = document.querySelector(".lowerValueInput");
+  const upperValueInput = document.querySelector(".upperValueInput");
 
   // Disable and make readonly by default
   searchInput.disabled = true;
   modelInput.disabled = true;
   minmaxrangeInput.disabled = true;
+  oneValueInput.disabled = true;
+  lowerValueInput.disabled = true;
+  upperValueInput.disabled = true;
 
   // Set default visibility to false
   searchElement.style.display = "none";
   modelElement.style.display = "none";
   minmaxrangeElement.style.display = "none";
+  oneValueElement.style.display = "none";
+  twoValueElement.style.display = "none";
 
   // Set visibility based on the selected filter
   switch (selectedFilter) {
