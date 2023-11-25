1577fc3228e3e188c934ba3e365c964a10caa1a3
Gabriel <gabrielfranz31@gmail.com>
2023-11-25 21:39:39 +0100

    fix

--- index.php
+++ index.php
@@ -13,8 +13,8 @@ include 'includes/mysql.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style/style.css">
     <script src="js/fullscreen.js"></script>
-    <script src="js/handleFilterChange.js"></script>
-    <script src="js/handleMinMaxRangeChange.js"></script>
+    <!--<script src="js/handleFilterChange.js"></script>
+    <script src="js/handleMinMaxRangeChange.js"></script>-->
     <script src="js/onLoad.js"></script>
 </head>
 
