8823595effb28f930a9ec5c91490f81b778fb510
Gabriel <gabrielfrnz31@gmail.com>
2023-11-27 15:35:21 +0100

    fix

--- js/duplicateForm.js
+++ js/duplicateForm.js
@@ -1,19 +1,21 @@
-document.addEventListener('click', function (event) {
-    var target = event.target;
-    if (target.classList.contains("remove-row")) {
-        console.log("Remove button clicked");
-        target.closest(".row").remove();
-    }
+function duplicateForm() {
+    document.addEventListener('click', function (event) {
+        var target = event.target;
+        if (target.classList.contains("remove-row")) {
+            console.log("Remove button clicked");
+            target.closest(".row").remove();
+        }
 
-    if (target.classList.contains("add-row")) {
-        console.log("Add button clicked");
-        const clonedElement = document
-            .querySelector("form > .container:first-child")
-            .cloneNode(true);
-        clonedElement.innerHTML +=
-            '<button type="button" class="remove-row btn btn-danger">Remove</button>';
-        document
-            .querySelector("form > .container:last-child")
-            .insertAdjacentElement("afterend", clonedElement);
-    }
-})
+        if (target.classList.contains("add-row")) {
+            console.log("Add button clicked");
+            const clonedElement = document
+                .querySelector("form > .container:first-child")
+                .cloneNode(true);
+            clonedElement.innerHTML +=
+                '<button type="button" class="remove-row btn btn-danger">Remove</button>';
+            document
+                .querySelector("form > .container:last-child")
+                .insertAdjacentElement("afterend", clonedElement);
+        }
+    })
+}
