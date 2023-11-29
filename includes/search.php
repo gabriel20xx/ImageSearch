98c208768c2f916de4a6a2adb806af9b4d88e5a9
Gabriel <gabrielfrnz31@gmail.com>
2023-11-29 10:45:36 +0100

    fix

--- includes/search.php
+++ includes/search.php
@@ -18,7 +18,7 @@ if (isset($_GET['search'])) {
         $value = 'LIKE ?';
     }
 
-    $sqlCount = "SELECT COUNT(*) as count FROM Metadata WHERE $filter $value";
+    $sqlCount = "SELECT COUNT(*) as count FROM Metadata WHERE $filter '$value'";
     $stmtCount = mysqli_prepare($conn, $sqlCount);
 
     if ($stmtCount) {
@@ -36,7 +36,7 @@ if (isset($_GET['search'])) {
         $totalCount = $row["count"];
         echo '<p class="text-center">Total number of results: ' . $totalCount . '</p>';
 
-        $sqlData = "SELECT * FROM Metadata WHERE $filter $value ORDER BY id $sort LIMIT ? OFFSET ?";
+        $sqlData = "SELECT * FROM Metadata WHERE $filter '$value' ORDER BY id $sort LIMIT ? OFFSET ?";
         $stmtData = mysqli_prepare($conn, $sqlData);
 
         if ($stmtData) {
